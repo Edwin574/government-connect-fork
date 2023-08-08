d468fba1197f2211093ced31aafb31deab25a560
Edwin574 <owinoedwin574@gmail.com>
2023-08-08 16:06:30 +0300

    dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import {} from "react";
 import "./App.css";
 import Sidenavigation from "./components/Sidenavigation";
-import Topbar from "./components/Topbar";
+// import Topbar from "./components/Topbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Outlet } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
       <Sidenavigation />
+      
       {/*<Topbar />*/}
       <Outlet />
     </div>
