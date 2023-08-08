0039278e28f9c51924198fe1951e73403050678a
Diana-nyamai <dnyamai.dn@gmail.com>
2023-08-08 12:38:04 +0300

    First nav done

--- src/components/Sidenavigation.jsx
+++ src/components/Sidenavigation.jsx
@@ -1,6 +1,7 @@
 import {  } from 'react';
 import '../styles/components/sidenav.css'
 import logo from '../assets/logo.png'
+import sakaja from '../assets/sakaja.png'
 import {BiSolidDashboard} from 'react-icons/bi'
 
 const Sidenavigation = ()=> { 
@@ -11,47 +12,65 @@ const Sidenavigation = ()=> {
         },
         {
             name: 'Emergency',
-            icon: ''
+            icon: <BiSolidDashboard/>
         },
         {
             name: 'Parking',
-            icon: 'dash'
+            icon: <BiSolidDashboard/>
         },
         {
             name: 'Business Permits',
-            icon: 'dash'
+            icon: <BiSolidDashboard/>
         },
         {
             name: 'Reporting incidences',
-            icon: 'dash'
+            icon: <BiSolidDashboard/>
         },
         {
             name: 'Public Spaces',
-            icon: 'dash'
+            icon: <BiSolidDashboard/>
         },
         {
             name: 'Make Payments',
-            icon: 'dash'
+            icon: <BiSolidDashboard/>
         },
     ]
   return (
     <div>
         <nav>
-            <ul>
-                <li><a href="#"><img className='logo' src={logo} alt="logo"/></a></li>
+            <div className='mainList'>
+                <div className='logoContainer'><a href="#"><img className='logo' src={logo} alt="logo"/></a></div>
+
                 {
                     navs.map((nav, index) =>
-                         <li key={index}>
+                         <div key={index} className='list'>
                             <a href="#">
                                 {nav.icon}
                                 <span className='nav-item'>{nav.name}
                                 </span>
                             </a>
-                         </li>
+                         </div>
                         )
                 }
                
-            </ul>
+               <hr className='line'/>
+
+               <div className='list'>
+                            <a href="#">
+                                <BiSolidDashboard/>
+                                <span className='nav-item'> Logout
+                                </span>
+                            </a>
+                         </div>
+            </div>
+
+            <div className='avatar'>
+                <div><img src={sakaja} alt="avatar"/></div>
+                <div className='text'>
+                    <p>Sakaja</p>
+                    <p>sakaja@gmail.com</p>
+                </div>
+            </div>
         </nav>
     </div>
   )
