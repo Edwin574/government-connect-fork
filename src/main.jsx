b2466f2aea89adb0af865067cd7d12ac84cc1047
Benjamin eBrainer <515ebrainer@gmail.com>
2023-08-08 11:34:01 +0300

    basic routing done

--- src/main.jsx
+++ src/main.jsx
@@ -4,26 +4,38 @@ import App from "./App.jsx";
 import "./index.css";
 import {
   Route,
+  RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
 } from "react-router-dom";
+import Dashboard from "./pages/dashboard.jsx";
+import Emergency from "./pages/Emergency.jsx";
+import Login from "./pages/Login.jsx";
+import Parking from "./pages/Parking.jsx";
+import Payments from "./pages/Payments.jsx";
+import Permits from "./pages/Permits.jsx";
+import PublicSpaces from "./pages/PublicSpaces.jsx";
+import ReportIncidence from "./pages/ReportIncidence.jsx";
+import Signup from "./pages/Signup.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/">
-      <Route />
-      <Route />
-      <Route />
-      <Route />
-      <Route />
-      <Route />
-      <Route />
+    <Route path="/" element={<App />}>
+      <Route index path="/" element={<Dashboard />} />
+      <Route path="emergency" element={<Emergency />} />
+      <Route path="login" element={<Login />} />
+      <Route path="parking" element={<Parking />} />
+      <Route path="payments" element={<Payments />} />
+      <Route path="permits" element={<Permits />} />
+      <Route path="public-spaces" element={<PublicSpaces />} />
+      <Route path="report-incidence" element={<ReportIncidence />} />
+      <Route path="signup" element={<Signup />} />
     </Route>
   )
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
