3a85e9d3220f280e174f1a8e0dd57ebaabee3e9e
Edwin574 <owinoedwin574@gmail.com>
2023-08-09 16:19:25 +0300

    made changes

--- src/pages/Emergency.jsx
+++ src/pages/Emergency.jsx
@@ -7,27 +7,56 @@ import Table from "../components/table";
 import Barchart from "../charts/Barchart";
 import { EmergencyData } from "../data/emergency";
 import { columns } from "../data/emergency";
-
-import "../styles/components/tables.css"
+import axios from "axios";
+import "../styles/components/tables.css";
 
 import { useDispatch, useSelector } from "react-redux";
-
+import { useEffect, useState } from "react";
 
 const Emergency = () => {
   const emergency = useSelector((state) => state.emergency.emergency);
   const dispatch = useDispatch();
-
+  const [ emdata, setemData ] = useState([]);
+  
+  useEffect(() => {
+    axios
+      .get("https://2440-41-84-159-198.ngrok-free.app/emergencies/read")
+      .then((res) => {
+        console.log(res);
+        setemData(res);
+      }).catch((error)=>{
+        console.log(error)
+      });
+  },[]);
+  // console.log('emergencydata',emdata)
   return (
     <div>
       <div className="mainCards">
-
-      <ShowCards icon={<LocalHospitalIcon/> } name="Emergency Services" numbers="300" percent={100} bg='light'/>
-      <ShowCards icon={<LocalParkingIcon/> } name="Solved Emergencies" numbers="200" percent={80} bg='normal'/>
-      <ShowCards icon={<AddBusinessTwoToneIcon/> } name="Pending Emergency" numbers="20" percent={20} bg='light'/>
+        <ShowCards
+          icon={<LocalHospitalIcon />}
+          name="Emergency Services"
+          numbers="300"
+          percent={100}
+          bg="light"
+        />
+        <ShowCards
+          icon={<LocalParkingIcon />}
+          name="Solved Emergencies"
+          numbers="200"
+          percent={80}
+          bg="normal"
+        />
+        <ShowCards
+          icon={<AddBusinessTwoToneIcon />}
+          name="Pending Emergency"
+          numbers="20"
+          percent={20}
+          bg="light"
+        />
       </div>
 
-      <div className='table-containter'>
-        <Table cols={columns} data={EmergencyData}/>
+      <div className="table-containter">
+        <Table cols={columns} data={EmergencyData} />
       </div>
 
       <div className="chart">
