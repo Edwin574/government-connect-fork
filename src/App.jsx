f788f20ee13420a984af7e10b00a4fe163f025d6
Edwin574 <owinoedwin574@gmail.com>
2023-08-09 09:20:31 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import {} from "react";
 import "./App.css";
-import { createTheme, ThemeProvider } from "@mui/material";
+// import { createTheme, ThemeProvider } from "@mui/material";
 import Sidenavigation from "./components/Sidenavigation";
 // import Topbar from "./components/Topbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,22 +9,7 @@ import { Outlet } from "react-router-dom";
 
 
 function App() {
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#00ff00",
-      },
-    },
-  });
-
   return (
-    <ThemeProvider theme={theme}>
-      <div className="app">
-        <Sidenavigation />
-        {/*<Topbar />*/}
-        <Outlet />
-      </div>
-    </ThemeProvider>
     <div className="app">
       <Sidenavigation />
       
